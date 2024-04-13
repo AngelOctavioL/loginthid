53c765c737edf2fc5bf12da2d65395ff52038fd2
Angel Lopez <a.o.lopez.cruz@icloud.com>
2024-04-12 20:50:59 -0600

    funcion text field

--- ThirdLoginBank/HomeViewController.swift
+++ ThirdLoginBank/HomeViewController.swift
@@ -8,20 +8,28 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-    
     @IBOutlet weak var loginButton: UIButton!
     @IBOutlet weak var userFiel: UITextField!
     @IBOutlet weak var passFiel: UITextField!
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if let homeNavigationController = segue.destination as?
-            UINavigationController {
-            if let userName = userFiel.text {
-                print("userFiel: \(String(describing: userFiel))")
-            }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loginButton.isEnabled = false
+    }
+    
+    @IBAction func changedTextFiel(_ sender: UITextField) {
+        print("changedUser: \(String(describing: userFiel.text))")
+        print("changedUser: \(String(describing: passFiel.text))")
+        if (((userFiel.text?.count) != nil) && ((passFiel.text?.count) != nil)) {
+            loginButton.isEnabled = true
         }
     }
+    
+//    @IBAction func changedPass(_ sender: UITextField) {
+//        print("changedPass")
+//    }
+    
+//    @IBAction func availableButton() {
+//        print("Aqui ==> \(String(describing: userFiel.text))")
+//    }
 }
